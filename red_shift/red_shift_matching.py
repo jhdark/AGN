176b648dfa2a54cc760fda6c2107b091aa357952
jhdark <james.dark.95@gmail.com>
2022-05-11 15:46:29 +0200

    updated graph ourputs

--- red_shift/red_shift_matching.py
+++ red_shift/red_shift_matching.py
@@ -1,9 +1,13 @@
+from cProfile import label
 from astropy.table import Table
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+
+plt.rc("text", usetex=True)
+plt.rc("font", family="serif", size=12)
 
 tab = Table.read("../COSMOS_zcat_forProject.fits", format="fits")  #  redshift table
 tab.colnames
@@ -67,57 +71,72 @@ def plot_data_from_file():
     return ra_values, dec_values, flux_values
 
 
-ra_values, dec_values = find_unique_obj_ra_dec_values()
-# ra_values, dec_values = plot_data_from_file()
+# ra_values, dec_values = find_unique_obj_ra_dec_values()
+ra_values, dec_values, flux_values = plot_data_from_file()
 
 
 # plot the coordinate of the objects on the sky
-# plt.figure()
-# plt.legend()
-# plt.xlabel("Ra")
-# plt.ylabel("Dec")
-# plt.title("our dataset")
+plt.figure()
+plt.title("Panstarrs dataset")
 # plt.plot(tab1["ra"], tab1["dec"], "k.", markersize=0.1)
+plt.plot(ra_values, dec_values, "k.", markersize=0.1)
+plt.xlabel(r"Ra ($\mathrm{^{\circ}}$)")
+plt.ylabel(r"Dec ($\mathrm{^{\circ}}$)")
+ax = plt.gca()
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
 
 
-# plt.figure()
-# plt.legend()
-# plt.xlabel("Ra")
-# plt.ylabel("Dec")
-# plt.title("both data")
-# plt.plot(tab["RA"], tab["DEC"], "r.", markersize=0.1, alpha=0.5)
+plt.figure()
+plt.title("Panstarrs dataset compared to redshift catalogue")
+plt.plot(
+    tab["RA"], tab["DEC"], "r.", label="Redshift catalogue", markersize=0.1, alpha=0.5
+)
 # plt.plot(tab1["ra"], tab1["dec"], "k.", markersize=0.1)
-
+plt.plot(ra_values, dec_values, "k.", label="Panstarrs dataset", markersize=0.1)
+plt.xlabel(r"Ra ($\mathrm{^{\circ}}$)")
+plt.ylabel(r"Dec ($\mathrm{^{\circ}}$)")
+# plt.xlim(149, 153)
+# plt.ylim(1, 4)
+ax = plt.gca()
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
+# Plot legend.
+lgnd = plt.legend(loc="upper right", numpoints=1, fontsize=10)
+lgnd.legendHandles[0]._legmarker.set_markersize(6)
+lgnd.legendHandles[1]._legmarker.set_markersize(6)
+plt.tight_layout()
+plt.show()
 # redshift table already has units for RA, DEC
 
-coords1 = SkyCoord(
-    tab["RA"], tab["DEC"]
-)  # redshift table already has units for RA, DEC
-coords2 = SkyCoord(ra_values, dec_values, unit=u.deg)
+# coords1 = SkyCoord(
+#     tab["RA"], tab["DEC"]
+# )  # redshift table already has units for RA, DEC
+# coords2 = SkyCoord(ra_values, dec_values, unit=u.deg)
 
-match, dist2d, dist3d = coords1.match_to_catalog_sky(coords2)
-print(np.array(match))
-print(dist2d)
-coords1[0]
-match[0]
-coords2[624568]
-dist2d[0].arcsec
+# match, dist2d, dist3d = coords1.match_to_catalog_sky(coords2)
+# print(np.array(match))
+# print(dist2d)
+# coords1[0]
+# match[0]
+# coords2[624568]
+# dist2d[0].arcsec
 
 # plt.figure()
 # plt.hist(
 #     dist2d.arcsec, bins=100, range=(0, 3)
 # )  # decided that optimal maximum matching distance is 0.5" for this table
 # (index,) = np.where(dist2d.arcsec < 0.5)
-coords1[index]
-coords2[match[index]]
+# coords1[index]
+# coords2[match[index]]
 
-header = ["Z_BEST", "dist2d"]
-rows = zip(tab["Z_BEST"], dist2d.degrees)
-with open("redshift_data.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    for row in rows:
-        writer.writerow(row)
+# header = ["Z_BEST", "dist2d"]
+# rows = zip(tab["Z_BEST"], dist2d.degrees)
+# with open("redshift_data.csv", "w", newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(header)
+#     for row in rows:
+#         writer.writerow(row)
 
 
 # (index,) = np.where(dist2d.arcsec < 0.5)
